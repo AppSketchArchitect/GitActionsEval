fe431bf2fbc3eef9a538f21a652abb2503668ebb
Kyrohe <enzoromoli44@gmail.com>
2025-11-16 19:54:43 +0100

    * Feat: Add .env for database variables * Feat: Add docker-compose to generate database

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOST,
+  MONGO_PORT,
+  MONGO_DB_NAME,
+} = process.env;
+
+const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`;
+
+function connectDB() {
+    return mongoose.connect(uri);
+}
+
+function disconnectDB() {
+    return mongoose.disconnect();
+}
+
+module.exports = { connectDB, disconnectDB }
