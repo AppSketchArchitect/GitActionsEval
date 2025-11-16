3e555416c382dd64f8c0e6ca82f250d62bd06295
Kyrohe <enzoromoli44@gmail.com>
2025-11-16 18:02:51 +0100

    * Fix: Code quality * Fix: Test import errors * Feat: Add .prettierignore to ignore coverage folder for formatting * Feat: Add coverage folder to .gitignore

--- tests/requestTypes.test.js
+++ tests/requestTypes.test.js
@@ -1,3 +1,4 @@
-test('smoke', () => { //Test bidon car aucun vrai tests pour l'instant
-  expect(true).toBe(true)
-})
+test('smoke', () => {
+  //Test bidon car aucun vrai tests pour l'instant
+  expect(true).toBe(true);
+});
